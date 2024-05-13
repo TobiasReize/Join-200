eb8560d9b7ec73f3d05ab6faa21423cc7429c547
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2024-05-13 17:33:00 +0200

    add simple dropdown menu

--- js/add_task.js
+++ js/add_task.js
@@ -1,1 +1,45 @@
 // Code oder Funktionen, die nur für die Add Task Seite relevant sind!
+
+let contacts = [
+    {
+        'first-name' : 'Thomas',
+        'name' : 'Mueller'
+    },
+    {
+        'first-name' : 'Bernd',
+        'name' : 'Schneider'
+    },
+    {
+        'first-name' : 'Michael',
+        'name' : 'Ballack'
+    },
+]
+
+
+function assignMenu() {
+    let content = document.getElementById('contactContainer');
+    content.classList.toggle('d-none');
+}
+
+function categoryMenu() {
+    let content = document.getElementById('categoryContainer');
+    content.classList.toggle('d-none');
+}
+
+function getContacts() {
+    let fullName = document.getElementById('fullName');
+    fullName.innerHTML = '';
+    
+    for (let i = 0; i < contacts.length; i++) {
+        const names = contacts[i];
+        
+        let name = names['name'];
+        let firstName = names['first-name'];
+        return firstName + name
+    }
+}
+
+function renderContacts() {
+    let content = document.getElementById('listedNames');
+    content.innerHTML = '';
+}
