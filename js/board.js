747e3b350d58f5701f1bca0a8b9cfaf7416020ec
Tobias Reize <reizeto@web.de>
2024-05-29 20:47:20 +0200

    updated bigView function, added closeBigView and deleteTask function

--- js/board.js
+++ js/board.js
@@ -1,23 +1,17 @@
-let low = `
-    <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="prio-low" d="M10.3555 9.69779C10.1209 9.69819 9.89234 9.62335 9.70349 9.48427L0.800382 2.91453C0.684543 2.82898 0.586704 2.72146 0.512448 2.59812C0.438193 2.47478 0.388977 2.33803 0.367609 2.19569C0.324455 1.90821 0.397354 1.61537 0.57027 1.3816C0.743187 1.14782 1.00196 0.992265 1.28965 0.949143C1.57734 0.906021 1.8704 0.978866 2.10434 1.15165L10.3555 7.23414L18.6066 1.15165C18.7224 1.0661 18.854 1.00418 18.9938 0.969432C19.1336 0.934685 19.2788 0.927791 19.4213 0.949143C19.5637 0.970495 19.7006 1.01967 19.824 1.09388C19.9474 1.16808 20.055 1.26584 20.1407 1.3816C20.2263 1.49735 20.2883 1.62882 20.323 1.7685C20.3578 1.90818 20.3647 2.05334 20.3433 2.19569C20.322 2.33803 20.2727 2.47478 20.1985 2.59812C20.1242 2.72146 20.0264 2.82898 19.9106 2.91453L11.0075 9.48427C10.8186 9.62335 10.5901 9.69819 10.3555 9.69779Z" fill="white"/>
-        <path class="prio-low" d="M10.3555 15.4463C10.1209 15.4467 9.89234 15.3719 9.70349 15.2328L0.800381 8.66307C0.566436 8.49028 0.410763 8.2317 0.367609 7.94422C0.324455 7.65674 0.397354 7.3639 0.57027 7.13013C0.743187 6.89636 1.00196 6.7408 1.28965 6.69768C1.57734 6.65456 1.8704 6.7274 2.10434 6.90019L10.3555 12.9827L18.6066 6.90019C18.8405 6.7274 19.1336 6.65456 19.4213 6.69768C19.709 6.7408 19.9678 6.89636 20.1407 7.13013C20.3136 7.3639 20.3865 7.65674 20.3433 7.94422C20.3002 8.2317 20.1445 8.49028 19.9106 8.66307L11.0075 15.2328C10.8186 15.3719 10.5901 15.4467 10.3555 15.4463Z" fill="white"/>
-    </svg>
-`;
-
-let medium = `
-    <svg width="21" height="8" viewBox="0 0 21 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="prio-medium" d="M19.7596 7.91717H1.95136C1.66071 7.91717 1.38197 7.80087 1.17645 7.59386C0.970928 7.38685 0.855469 7.10608 0.855469 6.81332C0.855469 6.52056 0.970928 6.23979 1.17645 6.03278C1.38197 5.82577 1.66071 5.70947 1.95136 5.70947H19.7596C20.0502 5.70947 20.329 5.82577 20.5345 6.03278C20.74 6.23979 20.8555 6.52056 20.8555 6.81332C20.8555 7.10608 20.74 7.38685 20.5345 7.59386C20.329 7.80087 20.0502 7.91717 19.7596 7.91717Z" fill="white"/>
-        <path class="prio-medium" d="M19.7596 2.67388H1.95136C1.66071 2.67388 1.38197 2.55759 1.17645 2.35057C0.970928 2.14356 0.855469 1.86279 0.855469 1.57004C0.855469 1.27728 0.970928 0.996508 1.17645 0.789496C1.38197 0.582485 1.66071 0.466187 1.95136 0.466187L19.7596 0.466187C20.0502 0.466187 20.329 0.582485 20.5345 0.789496C20.74 0.996508 20.8555 1.27728 20.8555 1.57004C20.8555 1.86279 20.74 2.14356 20.5345 2.35057C20.329 2.55759 20.0502 2.67388 19.7596 2.67388Z" fill="white"/>
-    </svg>
-`;
-
-let urgent = `
-    <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="prio-urgent" d="M19.2597 15.4464C19.0251 15.4468 18.7965 15.3719 18.6077 15.2328L10.3556 9.14965L2.10356 15.2328C1.98771 15.3184 1.85613 15.3803 1.71633 15.4151C1.57652 15.4498 1.43124 15.4567 1.28877 15.4354C1.14631 15.414 1.00944 15.3648 0.885997 15.2906C0.762552 15.2164 0.654943 15.1186 0.569314 15.0029C0.483684 14.8871 0.421712 14.7556 0.386936 14.6159C0.352159 14.4762 0.345259 14.331 0.366629 14.1887C0.409788 13.9012 0.565479 13.6425 0.799451 13.4697L9.70356 6.89926C9.89226 6.75967 10.1208 6.68433 10.3556 6.68433C10.5904 6.68433 10.819 6.75967 11.0077 6.89926L19.9118 13.4697C20.0977 13.6067 20.2356 13.7988 20.3057 14.0186C20.3759 14.2385 20.3747 14.4749 20.3024 14.6941C20.2301 14.9133 20.0904 15.1041 19.9031 15.2391C19.7159 15.3742 19.4907 15.4468 19.2597 15.4464Z" fill="white"/>
-        <path class="prio-urgent" d="M19.2597 9.69733C19.0251 9.69774 18.7965 9.62289 18.6077 9.48379L10.3556 3.40063L2.10356 9.48379C1.86959 9.6566 1.57651 9.72945 1.28878 9.68633C1.00105 9.6432 0.742254 9.48762 0.569318 9.25383C0.396382 9.02003 0.323475 8.72716 0.366634 8.43964C0.409793 8.15213 0.565483 7.89352 0.799455 7.72072L9.70356 1.15024C9.89226 1.01065 10.1208 0.935303 10.3556 0.935303C10.5904 0.935303 10.819 1.01065 11.0077 1.15024L19.9118 7.72072C20.0977 7.85763 20.2356 8.04974 20.3057 8.26962C20.3759 8.4895 20.3747 8.72591 20.3024 8.94509C20.2301 9.16427 20.0904 9.35503 19.9031 9.49012C19.7159 9.62521 19.4907 9.69773 19.2597 9.69733Z" fill="white"/>
-    </svg>
-`;
+let priorities = {
+    'low':      `<svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path class="prio-low" d="M10.3555 9.69779C10.1209 9.69819 9.89234 9.62335 9.70349 9.48427L0.800382 2.91453C0.684543 2.82898 0.586704 2.72146 0.512448 2.59812C0.438193 2.47478 0.388977 2.33803 0.367609 2.19569C0.324455 1.90821 0.397354 1.61537 0.57027 1.3816C0.743187 1.14782 1.00196 0.992265 1.28965 0.949143C1.57734 0.906021 1.8704 0.978866 2.10434 1.15165L10.3555 7.23414L18.6066 1.15165C18.7224 1.0661 18.854 1.00418 18.9938 0.969432C19.1336 0.934685 19.2788 0.927791 19.4213 0.949143C19.5637 0.970495 19.7006 1.01967 19.824 1.09388C19.9474 1.16808 20.055 1.26584 20.1407 1.3816C20.2263 1.49735 20.2883 1.62882 20.323 1.7685C20.3578 1.90818 20.3647 2.05334 20.3433 2.19569C20.322 2.33803 20.2727 2.47478 20.1985 2.59812C20.1242 2.72146 20.0264 2.82898 19.9106 2.91453L11.0075 9.48427C10.8186 9.62335 10.5901 9.69819 10.3555 9.69779Z" fill="white"/>
+                    <path class="prio-low" d="M10.3555 15.4463C10.1209 15.4467 9.89234 15.3719 9.70349 15.2328L0.800381 8.66307C0.566436 8.49028 0.410763 8.2317 0.367609 7.94422C0.324455 7.65674 0.397354 7.3639 0.57027 7.13013C0.743187 6.89636 1.00196 6.7408 1.28965 6.69768C1.57734 6.65456 1.8704 6.7274 2.10434 6.90019L10.3555 12.9827L18.6066 6.90019C18.8405 6.7274 19.1336 6.65456 19.4213 6.69768C19.709 6.7408 19.9678 6.89636 20.1407 7.13013C20.3136 7.3639 20.3865 7.65674 20.3433 7.94422C20.3002 8.2317 20.1445 8.49028 19.9106 8.66307L11.0075 15.2328C10.8186 15.3719 10.5901 15.4467 10.3555 15.4463Z" fill="white"/>
+                </svg>`,
+    'medium':  `<svg width="21" height="8" viewBox="0 0 21 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path class="prio-medium" d="M19.7596 7.91717H1.95136C1.66071 7.91717 1.38197 7.80087 1.17645 7.59386C0.970928 7.38685 0.855469 7.10608 0.855469 6.81332C0.855469 6.52056 0.970928 6.23979 1.17645 6.03278C1.38197 5.82577 1.66071 5.70947 1.95136 5.70947H19.7596C20.0502 5.70947 20.329 5.82577 20.5345 6.03278C20.74 6.23979 20.8555 6.52056 20.8555 6.81332C20.8555 7.10608 20.74 7.38685 20.5345 7.59386C20.329 7.80087 20.0502 7.91717 19.7596 7.91717Z" fill="white"/>
+                    <path class="prio-medium" d="M19.7596 2.67388H1.95136C1.66071 2.67388 1.38197 2.55759 1.17645 2.35057C0.970928 2.14356 0.855469 1.86279 0.855469 1.57004C0.855469 1.27728 0.970928 0.996508 1.17645 0.789496C1.38197 0.582485 1.66071 0.466187 1.95136 0.466187L19.7596 0.466187C20.0502 0.466187 20.329 0.582485 20.5345 0.789496C20.74 0.996508 20.8555 1.27728 20.8555 1.57004C20.8555 1.86279 20.74 2.14356 20.5345 2.35057C20.329 2.55759 20.0502 2.67388 19.7596 2.67388Z" fill="white"/>
+                </svg>`,
+    'urgent':  `<svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path class="prio-urgent" d="M19.2597 15.4464C19.0251 15.4468 18.7965 15.3719 18.6077 15.2328L10.3556 9.14965L2.10356 15.2328C1.98771 15.3184 1.85613 15.3803 1.71633 15.4151C1.57652 15.4498 1.43124 15.4567 1.28877 15.4354C1.14631 15.414 1.00944 15.3648 0.885997 15.2906C0.762552 15.2164 0.654943 15.1186 0.569314 15.0029C0.483684 14.8871 0.421712 14.7556 0.386936 14.6159C0.352159 14.4762 0.345259 14.331 0.366629 14.1887C0.409788 13.9012 0.565479 13.6425 0.799451 13.4697L9.70356 6.89926C9.89226 6.75967 10.1208 6.68433 10.3556 6.68433C10.5904 6.68433 10.819 6.75967 11.0077 6.89926L19.9118 13.4697C20.0977 13.6067 20.2356 13.7988 20.3057 14.0186C20.3759 14.2385 20.3747 14.4749 20.3024 14.6941C20.2301 14.9133 20.0904 15.1041 19.9031 15.2391C19.7159 15.3742 19.4907 15.4468 19.2597 15.4464Z" fill="white"/>
+                    <path class="prio-urgent" d="M19.2597 9.69733C19.0251 9.69774 18.7965 9.62289 18.6077 9.48379L10.3556 3.40063L2.10356 9.48379C1.86959 9.6566 1.57651 9.72945 1.28878 9.68633C1.00105 9.6432 0.742254 9.48762 0.569318 9.25383C0.396382 9.02003 0.323475 8.72716 0.366634 8.43964C0.409793 8.15213 0.565483 7.89352 0.799455 7.72072L9.70356 1.15024C9.89226 1.01065 10.1208 0.935303 10.3556 0.935303C10.5904 0.935303 10.819 1.01065 11.0077 1.15024L19.9118 7.72072C20.0977 7.85763 20.2356 8.04974 20.3057 8.26962C20.3759 8.4895 20.3747 8.72591 20.3024 8.94509C20.2301 9.16427 20.0904 9.35503 19.9031 9.49012C19.7159 9.62521 19.4907 9.69773 19.2597 9.69733Z" fill="white"/>
+                </svg>`
+}
 
 let colorsCategories = {
     'Technical Task': '#1FD7C1',
@@ -39,7 +33,7 @@ let inProgressTasks = [
     {
         'category': 'User Story',
         'title': 'Daily Kochwelt Recipe',
-        'description': 'Implement daily recipe and portion calculator...',
+        'description': 'Implement daily recipe and portion calculator',
         'subtasks': [
             {
             'subtaskTitle': 'Subtask 1',
@@ -60,7 +54,7 @@ let inProgressTasks = [
             }
         ],
         'date': '2024-05-02',
-        'priority': medium
+        'priority': 'medium'
     }
 ];
 let awaitFeedbackTasks = [
@@ -84,12 +78,12 @@ let awaitFeedbackTasks = [
             }
         ],
         'date': '2023-06-09',
-        'priority': low
+        'priority': 'low'
     },
     {
         'category': 'User Story',
         'title': 'Daily Kochwelt Recipe',
-        'description': 'Implement daily recipe and portion calculator...',
+        'description': 'Implement daily recipe and portion calculator',
         'subtasks': [
             {
             'subtaskTitle': 'Subtask 1',
@@ -118,7 +112,7 @@ let awaitFeedbackTasks = [
             }
         ],
         'date': '2024-07-18',
-        'priority': urgent
+        'priority': 'urgent'
     }
 ];
 let doneTasks = [];
@@ -248,7 +242,7 @@ function renderColumn(columnTitle, columnID, columnArray) {     //Rendert die je
             for (let k = 0; k < task['contacts'].length; k++) {
                 const personFirstName = task['contacts'][k]['firstName'];
                 const personLastName = task['contacts'][k]['lastName'];
-                const initials = `${firstLetterUppercase(personFirstName)}${firstLetterUppercase(personLastName)}`;
+                const initials = `${oneLetterUppercase(personFirstName)}${oneLetterUppercase(personLastName)}`;
                 document.getElementById(`${columnID}_contacts_container_${i}`).innerHTML += `<div class="initials">${initials}</div>`;
             }
         }
@@ -265,15 +259,10 @@ function taskCardHTML(columnID, i, task) {  //HTML-Template für ausgefüllte Ka
             <div id="${columnID}_subtasks_container_${i}" class="subtasks-container"></div>
             <div class="ai-ctr-space-btwn">
                 <div id="${columnID}_contacts_container_${i}" class="df-ai-ctr"></div>
-                ${task['priority']}
+                ${priorities[task['priority']]}
             </div>
         </div>
     `;
-}
-
-
-function firstLetterUppercase(word) {           //Hilfsfunktion, damit der erste Buchstabe großgeschrieben wird
-    return word.charAt(0).toUpperCase();
 }
 
 
@@ -284,10 +273,10 @@ function showBigView(columnID, index) {
     
     bigViewContainer.classList.remove('d-none');
     bigViewContainer.innerHTML = /*html*/ `
-        <div class="big-view-card" onclick="stopPropagation(event)">
+        <div id="big_view_card" class="big-view-card" onclick="stopPropagation(event)">
             <div class="ai-ctr-space-btwn m-btm24">
                 <div class="big-view-category" style="background-color: ${colorsCategories[currentTask['category']]}">${currentTask['category']}</div>
-                <svg onclick="closeBigView()" class="big-view-close" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg onclick="closeBigView(0)" class="big-view-close" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <mask id="mask0_71720_5473" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
                         <rect y="0.96582" width="24" height="24" fill="#D9D9D9"/>
                     </mask>
@@ -304,7 +293,10 @@ function showBigView(columnID, index) {
             </div>
             <div class="big-view-priority-container df-ai-ctr fs20-fw400 m-btm24">
                 <div class="default-color">Priority:</div>
-                <div>${currentTask['priority']}</div>
+                <div class="df-ai-ctr gap-10">
+                    <div>${firstLetterUppercase(currentTask['priority'])}</div>
+                    <div>${priorities[currentTask['priority']]}</div>
+                </div>
             </div>
             <div id="big_view_contacts_container" class="m-btm24">
                 <div class="default-color fs20-fw400">Assigned To:</div>
@@ -314,7 +306,7 @@ function showBigView(columnID, index) {
             </div>
             <div class="big-view-lowest-container df-ai-ctr">
                 <div class="big-view-delete-edit-container df-ai-ctr">
-                    <div class="big-view-delete-container df-ai-ctr">
+                    <div class="big-view-delete-container df-ai-ctr" onclick="deleteTask(${columnID['id']}, ${index})">
                         <svg width="24" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <mask id="mask0_75592_9951" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="24">
                                 <rect x="0.144531" width="24" height="24" fill="#D9D9D9"/>
@@ -343,12 +335,16 @@ function showBigView(columnID, index) {
     `;
 
     //fügt die Kontakte der Großansicht hinzu:
-    // for (let i = 0; i < currentTask['contacts'].length; i++) {
-    //     const person = currentTask['contacts'][i];
-    //     const personFirstName = person['firstName'];
-    //     const personLastName = person['lastName'];
-    //     const initials = `${firstLetterUppercase(personFirstName)}${firstLetterUppercase(personLastName)}`;
-    // }
+    for (let i = 0; i < currentTask['contacts'].length; i++) {
+        const person = currentTask['contacts'][i];
+        const initials = `${oneLetterUppercase(person['firstName'])}${oneLetterUppercase(person['lastName'])}`;
+        document.getElementById('big_view_contacts_container').innerHTML += /*html*/ `
+            <div class="big-view-contacts df-ai-ctr">
+                <div class="big-view-initials">${initials}</div>
+                <div class="big-view-contacts-name">${person['firstName']} ${person['lastName']}</div>
+            </div>
+        `;
+    }
 
     //fügt die Subtasks der Großansicht hinzu:
     for (let j = 0; j < currentTask['subtasks'].length; j++) {
@@ -360,22 +356,20 @@ function showBigView(columnID, index) {
             </div>
         `;
     }
+
+    //lässt die Karte von rechts reinfliegen:
+    setTimeout(() => document.getElementById('big_view_card').classList.add('translateX-0'), 100);  //Verzögerung damit die Karte erst außerhalb des Bildschirms dargestellt wird und dann reinfliegt!
 }
 
 
-function closeBigView() {
-    let bigViewContainer = document.getElementById('big_view_container');
-    bigViewContainer.classList.add('d-none');
-}
-
-
-function stopPropagation(event) {           //Verhindert das Event Bubbling beim Schließen der Großansicht
-    event.stopPropagation();
-}
-
-
-function allowDrop(event) {    //Funktion, die das Fallenlassen (Drop) in andere Elemente ermöglicht (ist standardmäßig nicht erlaubt, daher muss es umgestellt werden!)
-    event.preventDefault();
+function closeBigView(id) {       //Schließt die Großansicht wieder
+    if (id == 0) {      //zur Fall-Unterscheidung: Kenner "0" kommt vom Schließen der Großansicht (Animation wird ausgeführt)
+        document.getElementById('big_view_card').classList.remove('translateX-0');
+        setTimeout(() => document.getElementById('big_view_container').classList.add('d-none'),300);    //zur Verzögerung für die Animation
+    } else {    //Kenner "1" kommt vom Löschen der Aufgabe (Animation wird nicht ausgeführt)
+        document.getElementById('big_view_container').classList.add('d-none');
+        document.getElementById('big_view_card').classList.remove('translateX-0');
+    }
 }
 
 
@@ -416,6 +410,16 @@ function endRotateCard(columnID, taskID) {      //Karte wird wieder in den Ausga
 }
 
 
+function deleteTask(columnID, index) {
+    let currentArrayIndex = renderedBoardArrays.findIndex(element => element['id'] == columnID['id']);
+    let currentArray = renderedBoardArrays[currentArrayIndex]['array'];
+    // let currentTask = currentArray[index];
+    currentArray.splice(index, 1);
+    closeBigView(1);
+    renderAll();
+}
+
+
 function searchTasks() {    //Funktion für die Suchleiste. Die gerenderten Arrays werden gefiltert und in ein extra-Array gespeichert!
     let searchField = document.getElementById('search_field');
     let searchFieldInput = searchField.value.trim().toLowerCase();
@@ -440,4 +444,25 @@ function searchTasks() {    //Funktion für die Suchleiste. Die gerenderten Arra
     } else if (searchFieldInput.length == 0) {      //falls die Eingabe wieder gelöscht wird
         renderAll();
     }
+}
+
+
+// Hilfsfunktionen:
+function stopPropagation(event) {           //Verhindert das Event Bubbling beim Schließen der Großansicht
+    event.stopPropagation();
+}
+
+
+function allowDrop(event) {                 //Funktion, die das Fallenlassen (Drop) in andere Elemente ermöglicht (ist standardmäßig nicht erlaubt, daher muss es umgestellt werden!)
+    event.preventDefault();
+}
+
+
+function oneLetterUppercase(word) {           //Hilfsfunktion, die nur den ersten Buchstaben großgeschrieben zurückgibt
+    return word.charAt(0).toUpperCase();
+}
+
+
+function firstLetterUppercase(word) {           //Hilfsfunktion, die den ersten Buchstaben großschreibt und dann das ganze Wort wieder zurückgibt
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
