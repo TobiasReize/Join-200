69abf86dcf9a5963b3babc996098c79318e1b25c
Tobias Reize <reizeto@web.de>
2024-06-20 20:51:36 +0200

    correct BASE_URL

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,4 @@
-const BASE_URL = 'https://join-200-default-rtdb.europe-west1.firebasedatabase.app/';
-// Mein: https://join-200-default-rtdb.europe-west1.firebasedatabase.app/
-// Patrick: https://join-200-81e6b-default-rtdb.europe-west1.firebasedatabase.app/
+const BASE_URL = 'https://join-200-81e6b-default-rtdb.europe-west1.firebasedatabase.app/';
 
 async function setItem(path = '', value) {          //Neuer User wird in der Datenbank, mit allen Daten, gespeichert! --> path wird vorab als String deklariert
     let response = await fetch(BASE_URL + path + '.json', {
@@ -20,11 +18,13 @@ async function loadData(path = '', importArray) {
     importArray =  importArray.push(...responseToJson); // Daten aus der Datenbank werden in Array gespeichert
 }
 
+
 async function getItem(path = '') {
     let response = await fetch(BASE_URL + path + '.json');
     let responseToJson = await response.json();
     return responseToJson;
 }
+
 
 async function loadContacts(path = '') {
     let response = await fetch(BASE_URL + path + '.json');
