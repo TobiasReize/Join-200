af7ac5596a764eeb4d628e9ae1e03199ab933eff
Robin Oester-Barkey <robin.info@oester-b.com>
2024-06-21 11:14:07 +0200

    new

--- js/summary.js
+++ js/summary.js
@@ -1,45 +1,51 @@
-// Es wird gewartet bis die Seite komplet fertig geladen ist, sonst kommt es zu überschneidungen mit der templates.js und dadurch wird der header / navbar nicht erzeugt
+// Globale Konstante für den Standardnamen
+const DEFAULT_NAME = 'guest';
+
 document.addEventListener('DOMContentLoaded', function() {
-    letsGreet();
-    loadNumber();
+    initializeApp();
 });
 
+async function initializeApp() {
+    await loadTasksFromFirebase();
+    letsGreet();
+    loadNumber();
+};
 
-// Beim Laden der Seite den Namen aus der URL extrahieren und greet-Funktion aufrufen
 async function letsGreet() {
     let userName = getQueryParameter('name');
+    
+    // Versuche den Namen aus dem localStorage zu lesen
+    let storedName = localStorage.getItem('userName');
+    
     if (userName !== null) {
         await greet(userName);
+        // Speichere den Namen im localStorage, falls er über die URL übergeben wurde
+        localStorage.setItem('userName', userName);
+    } else if (storedName !== null) {
+        await greet(storedName);
     } else {
-        // Wenn kein Name gefunden wird, soll ein Standardname 'guest' verwendet werden
-        await greet('guest');
+        await greet(DEFAULT_NAME); // Standardname verwenden
     }
-}
+};
 
-
-// Funktion zum Auslesen des Query-Parameters
 function getQueryParameter(name) {
     let params = new URLSearchParams(window.location.search);
     return params.get(name);
-}
+};
 
-
-// Greet-Funktion
 async function greet(userName) {
     const greeting = await getGreeting();
     let greetingText;
     
-    if (userName == 'guest') {
+    if (userName === DEFAULT_NAME) {
         greetingText = `<p>${greeting}!</p>`;
     } else {
         greetingText = `<p>${greeting},<br> ${userName}!</p>`;
     }
     
     document.getElementById('greeting').innerHTML = greetingText;
-}
+};
 
-
-// Funktion zur Ermittlung der aktuellen Begrüßung
 function getGreeting() {
     return new Promise(resolve => {
         const now = new Date();
@@ -56,16 +62,52 @@ function getGreeting() {
 
         resolve(greeting);
     });
-}
+};
 
+let toDoTasks = [];
+let inProgressTasks = [];
+let awaitFeedbackTasks = [];
+let doneTasks = [];
 
-function loadNumber() {
-    todoNumber = toDoTasks.length
-    doneNumber = doneTasks.length
-    awaitFeedbackNumber = awaitFeedbackTasks.length
-    inProgressNumber = inProgressTasks.length
-    document.getElementById('todoNumber').innerHTML = `${todoNumber}`
-    document.getElementById('doneNumber').innerHTML = `${doneNumber}`
-    document.getElementById('awaitFeedbackNumber').innerHTML = `${awaitFeedbackNumber}`
-    document.getElementById('inProgressNumber').innerHTML = `${inProgressNumber}`
-}
+async function sortTasks() {
+    for (let i = 0; i < importTasks.length; i++) {
+        const singleTask = importTasks[i];
+
+        switch (singleTask.columnID) {
+            case 'ToDo':
+                toDoTasks.push(singleTask);
+                break;
+                
+            case 'InProgress':
+                inProgressTasks.push(singleTask);
+                break;
+                
+            case 'AwaitFeedback':
+                awaitFeedbackTasks.push(singleTask);
+                break;
+            
+            case 'Done':
+                doneTasks.push(singleTask);
+                break;
+        }
+    }
+};
+
+async function loadNumber() {
+    await sortTasks();
+    const todoNumber = toDoTasks.length;
+    const doneNumber = doneTasks.length;
+    const awaitFeedbackNumber = awaitFeedbackTasks.length;
+    const inProgressNumber = inProgressTasks.length;
+    const sum = toDoTasks.length + doneTasks.length + awaitFeedbackTasks.length + inProgressTasks.length;
+
+    document.getElementById('todoNumber').innerHTML = `${todoNumber}`;
+    document.getElementById('doneNumber').innerHTML = `${doneNumber}`;
+    document.getElementById('awaitFeedbackNumber').innerHTML = `${awaitFeedbackNumber}`;
+    document.getElementById('inProgressNumber').innerHTML = `${inProgressNumber}`;
+    document.getElementById('taskSum').innerHTML = `${sum}`;
+};
+
+async function loadTasksFromFirebase() {
+    await loadData('tasks', importTasks);
+};
